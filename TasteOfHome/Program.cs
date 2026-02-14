f4eafbc0ab04abd73a962e02c23afb2cb73fce67
Jaimin158 <Jaiminsolanki05@gmail.com>
2026-02-14 13:14:13 -0500

    Added google auth login

--- TasteOfHome/Program.cs
+++ TasteOfHome/Program.cs
@@ -1,5 +1,9 @@
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.Google;
 using Microsoft.EntityFrameworkCore;
 using TasteOfHome.Data;
+
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -10,6 +14,20 @@ builder.Services.AddHttpClient();
 
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlite("Data Source=tasteofhome.db"));
+
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = GoogleDefaults.AuthenticationScheme;
+})
+.AddCookie()
+.AddGoogle(options =>
+{
+    options.ClientId = builder.Configuration["Authentication:Google:ClientId"]!;
+    options.ClientSecret = builder.Configuration["Authentication:Google:ClientSecret"]!;
+    options.SaveTokens = true;
+});
+builder.Services.AddAuthorization();
 
 var app = builder.Build();
 
@@ -27,12 +45,28 @@ if (!app.Environment.IsDevelopment())
     app.UseExceptionHandler("/Error");
     app.UseHsts();
 }
+app.MapGet("/login-google", (string? returnUrl) =>
+{
+    return Results.Challenge(
+        new Microsoft.AspNetCore.Authentication.AuthenticationProperties
+        {
+            RedirectUri = string.IsNullOrWhiteSpace(returnUrl) ? "/" : returnUrl
+        },
+        new[] { "Google" }
+    );
+});
+    
 
+app.MapGet("/logout", async (HttpContext ctx) =>
+{
+    await ctx.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+    return Results.Redirect("/");
+});
 app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
-
+app.UseAuthentication();
 app.UseAuthorization();
 
 // Map endpoints AFTER routing
