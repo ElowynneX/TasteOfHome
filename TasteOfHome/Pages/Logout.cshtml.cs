1286a97b5c227c8e10f004520a2fa45398bfc036
Joy Patel <pateljoy149@gmail.com>
2026-02-20 20:54:25 -0500

    UI Enhanced Project for Sprint2

--- TasteOfHome/Pages/Logout.cshtml.cs
+++ TasteOfHome/Pages/Logout.cshtml.cs
@@ -1,14 +1,19 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using TasteOfHome.Data;
+using Microsoft.AspNetCore.Authentication;
 
 namespace TasteOfHome.Pages
 {
     public class LogoutModel : PageModel
     {
-        public IActionResult OnPost()
+        public async Task<IActionResult> OnPostAsync()
         {
             FakeUsers.LoggedInEmail = null;
+
+            await HttpContext.SignOutAsync("Cookies");
+            await HttpContext.SignOutAsync("Google");
+
             return RedirectToPage("/Index");
         }
     }
